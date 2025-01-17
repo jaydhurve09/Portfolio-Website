7e3636ea9965a4f98b987a42afc5c0502e9db29e
jaydhurve09 <jaydhurve09@gmail.com>
2025-01-17 13:06:45 +0530

    Navbar created

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,21 +5,28 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <nav>
-        <a href="">
-            <Image src={assets.logo} className='w-28 cursor-pointer mr-14'/>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    </div>
+    <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between'>
+        <a href="#top">
+            <Image src={assets.logo} alt='jay-logo' className='w-28 cursor-pointer mr-14'/>
         </a>
 
-        <ul>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About Me</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">My Works</a></li>
-            <li><a href="#contact">Contact Me</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+            <li><a href="#top" className='font-Ovo'>Home</a></li>
+            <li><a href="#about" className='font-Ovo'>About Me</a></li>
+            <li><a href="#services" className='font-Ovo'>Services</a></li>
+            <li><a href="#work" className='font-Ovo'>My Works</a></li>
+            <li><a href="#contact" className='font-Ovo'>Contact Me</a></li>
         </ul>
 
-        <div>
-            <a href="#contact">Contact <Image src={assets.arrow_icon}/></a>
+        <div className='flex items-center gap-4'>
+            <button>
+                <Image src={assets.moon_icon} alt='' className='w-6'/>
+            </button>
+
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
         </div>
     </nav>
     </>
