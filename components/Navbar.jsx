f789660695b534d3ad62a3c439d040d8c4274ce6
jaydhurve09 <jaydhurve09@gmail.com>
2025-01-17 14:03:19 +0530

    Navbar.jsx updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+    const sideMenuRef = useRef()
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -27,7 +37,25 @@ const Navbar = () => {
             </button>
 
             <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
+
+            <button className='block md:hidden ml-3' onClick={openMenu}>
+            <Image src={assets.menu_black} alt='' className='w-6'/>
+            </button>
         </div>
+
+        {/* --------Mobile-View-------- */}
+
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 px-10 py-20 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+            <div className='absolute right-6 top-6' onClick={closeMenu}>
+                <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+            </div>
+
+            <li><a onClick={closeMenu} href="#top" className='font-Ovo'>Home</a></li>
+            <li><a onClick={closeMenu} href="#about" className='font-Ovo'>About Me</a></li>
+            <li><a onClick={closeMenu} href="#services" className='font-Ovo'>Services</a></li>
+            <li><a onClick={closeMenu} href="#work" className='font-Ovo'>My Works</a></li>
+            <li><a onClick={closeMenu} href="#contact" className='font-Ovo'>Contact Me</a></li>
+        </ul>
     </nav>
     </>
   )
