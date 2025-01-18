a6ae8f5d85eced4a90397e3b95ac2df67648ba32
jaydhurve09 <jaydhurve09@gmail.com>
2025-01-18 10:44:43 +0530

    Header and About created.

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto flex flex-col items-center justify-center gap-4 h-screen'>
+        <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
+        </div>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi I&apos;m Jay Dhurve <Image src={assets.hand_icon} alt='' className='w-6' /></h3>
+
+        <h1 className='font-Ovo text-3xl sm:text-6xl lg:text-[66px]'>frontend web developer based in India</h1>
+        <p className='max-w-2xl mx-auto font-Ovo'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem voluptate dignissimos facere error fugiat expedita obcaecati ipsum optio neque! Hic.</p>
+
+        <div className='flex flex-col sm:flex-row items center gap-4 mt-4 '>
+            <a href="#contact" className='px-10 py-3 border rounded-full border-gray-500 bg-black text-white flex items-center gap-2'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
+
+            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume <Image src={assets.download_icon} alt='' className='w-4' /></a>
+        </div>
+    </div>
+  )
+}
+
+
+export default Header
